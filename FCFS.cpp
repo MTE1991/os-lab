5ed6fabbe687c17ba6d67a0418e789d43a94d880
MT Ekleel <mtahsinekleel02@gmail.com>
2025-01-30 10:10:09 +0600

    Update FCFS.cpp

--- FCFS.cpp
+++ FCFS.cpp
@@ -1,3 +1,4 @@
+// C++ implementation of the FCFS algorithm
 #include <cstdlib>
 #include <iostream>
 #include <queue>
@@ -6,43 +7,71 @@ using namespace std;
 class process {
 public:
     pid_t p_no = 0;
-    time_t start_AT = 0, AT = 0, BT_left = 0, BT = 0, temp_BT = 0, CT = 0, TAT = 0, WT = 0, RT = 0;
+    time_t start_AT = 0, AT = 0,
+           BT_left = 0, BT = 0, temp_BT = 0,
+           CT = 0, TAT = 0, WT = 0, RT = 0;
     int priority = 0;
 
-    void set_CT(time_t time) {
+    // Function for completion time
+    void set_CT(time_t time)
+    {
         CT = time;
         set_TAT();
         set_WT();
     }
 
-    void set_TAT() {
+    // Function for Turn Around Time
+    void set_TAT()
+    {
         TAT = CT - start_AT;
     }
 
-    void set_WT() {
+    // Function for Waiting Time
+    void set_WT()
+    {
         WT = TAT - BT;
     }
-
-    void P_set() {
+    // Function to set starting Arrival Time
+    // Because arrival time gets updated
+    // when you push process in ready queue again
+    // in preemptive algorithms
+    void P_set()
+    {
         start_AT = AT;
         BT_left = BT;
     }
-
-    void set_RT(time_t time) {
+    // Function to set Response Time
+    void set_RT(time_t time)
+    {
         RT = time - start_AT;
     }
 
-    friend bool operator<(const process& a, const process& b) {
+    // Overload operator '<' w.r.t arrival
+    // time because arrival time is the
+    // first priority even greater than
+    // priority of process and priority_queue
+    // pops out the greatest value first
+    // so we need to replace '<' with '>' inorder
+    // to pop out smallest value
+    friend bool operator<(const process& a, const process& b)
+    {
         return a.AT > b.AT;
     }
 };
 
-priority_queue<process> FCFS_run(priority_queue<process> ready_queue, queue<process>* gantt) {
+// Function to implement FCFS algorithm
+priority_queue<process> FCFS_run(priority_queue<process> ready_queue,
+                                 queue<process>* gantt)
+{
     priority_queue<process> completion_queue;
     process p;
     time_t clock = 0;
 
+    // Till ready queue is not empty
     while (!ready_queue.empty()) {
+
+        // While clock is less than
+        // Arrival Time
         while (clock < ready_queue.top().AT) {
             p.temp_BT++;
             clock++;
@@ -61,31 +90,71 @@ priority_queue<process> FCFS_run(priority_queue<process> ready_queue, queue<proc
             clock++;
         }
         p.set_CT(clock);
+
+        // Update the Gantt Chart
         (*gantt).push(p);
         p.temp_BT = 0;
+
+        // Update the completion time to
+        // the queue
         completion_queue.push(p);
     }
     return completion_queue;
 }
 
-priority_queue<process> set_sample_data() {
+// Set data on the basis of given table
+priority_queue<process> set_sample_data()
+{
     priority_queue<process> ready_queue;
     process temp;
-    int process_data[7][3] = {
-        {0, 4, 2}, {1, 2, 4}, {2, 3, 6}, {3, 5, 10}, {4, 1, 8}, {5, 4, 12}, {6, 6, 9}};
-
-    for (int i = 0; i < 7; i++) {
-        temp.AT = process_data[i][0];
-        temp.BT = process_data[i][1];
-        temp.priority = process_data[i][2];
-        temp.p_no = i + 1;
-        temp.P_set();
-        ready_queue.push(temp);
-    }
+    temp.AT = 0;
+    temp.BT = 4;
+    temp.priority = 2;
+    temp.p_no = 1;
+    temp.P_set();
+    ready_queue.push(temp);
+    temp.AT = 1;
+    temp.BT = 2;
+    temp.priority = 4;
+    temp.p_no = 2;
+    temp.P_set();
+    ready_queue.push(temp);
+    temp.AT = 2;
+    temp.BT = 3;
+    temp.priority = 6;
+    temp.p_no = 3;
+    temp.P_set();
+    ready_queue.push(temp);
+    temp.AT = 3;
+    temp.BT = 5;
+    temp.priority = 10;
+    temp.p_no = 4;
+    temp.P_set();
+    ready_queue.push(temp);
+    temp.AT = 4;
+    temp.BT = 1;
+    temp.priority = 8;
+    temp.p_no = 5;
+    temp.P_set();
+    ready_queue.push(temp);
+    temp.AT = 5;
+    temp.BT = 4;
+    temp.priority = 12;
+    temp.p_no = 6;
+    temp.P_set();
+    ready_queue.push(temp);
+    temp.AT = 6;
+    temp.BT = 6;
+    temp.priority = 9;
+    temp.p_no = 7;
+    temp.P_set();
+    ready_queue.push(temp);
     return ready_queue;
 }
 
-double get_total_WT(priority_queue<process> processes) {
+// Function to get total Waiting Time
+double get_total_WT(priority_queue<process> processes)
+{
     double total = 0;
     while (!processes.empty()) {
         total += processes.top().WT;
@@ -94,7 +163,9 @@ double get_total_WT(priority_queue<process> processes) {
     return total;
 }
 
-double get_total_TAT(priority_queue<process> processes) {
+// Function to get total Turn Around Time
+double get_total_TAT(priority_queue<process> processes)
+{
     double total = 0;
     while (!processes.empty()) {
         total += processes.top().TAT;
@@ -103,7 +174,9 @@ double get_total_TAT(priority_queue<process> processes) {
     return total;
 }
 
-double get_total_CT(priority_queue<process> processes) {
+// Function to get total Completion Time
+double get_total_CT(priority_queue<process> processes)
+{
     double total = 0;
     while (!processes.empty()) {
         total += processes.top().CT;
@@ -112,7 +185,9 @@ double get_total_CT(priority_queue<process> processes) {
     return total;
 }
 
-double get_total_RT(priority_queue<process> processes) {
+// Function to get total Response Time
+double get_total_RT(priority_queue<process> processes)
+{
     double total = 0;
     while (!processes.empty()) {
         total += processes.top().RT;
@@ -121,66 +196,166 @@ double get_total_RT(priority_queue<process> processes) {
     return total;
 }
 
-void disp(priority_queue<process> main_queue, bool high) {
-    int size = main_queue.size();
+// Function to display Completion Queue and
+// all the time
+void disp(priority_queue<process> main_queue, bool high)
+{
+    int i = 0, temp, size = main_queue.size();
     priority_queue<process> tempq = main_queue;
     double temp1;
-    
-    cout << "\nTotal completion time: " << (temp1 = get_total_CT(tempq)) << "\n";
-    cout << "Average completion time: " << temp1 / size << "\n";
-    cout << "Total turnaround time: " << (temp1 = get_total_TAT(tempq)) << "\n";
-    cout << "Average turnaround time: " << temp1 / size << "\n";
-    cout << "Total waiting time: " << (temp1 = get_total_WT(tempq)) << "\n";
-    cout << "Average waiting time: " << temp1 / size << "\n";
-    cout << "Total response time: " << (temp1 = get_total_RT(tempq)) << "\n";
-    cout << "Average response time: " << temp1 / size << "\n";
+    cout << "+-------------+--------------";
+    cout << "+------------+-----------------";
+    cout << "+-----------------+--------------+---------------+";
+    if (high == true)
+        cout << "----------+" << endl;
+    else
+        cout << endl;
+    cout << "| Process No. | Arrival Time ";
+    cout << "| Burst Time | Completion Time ";
+    cout << "| Turnaround Time | Waiting Time | Response Time |";
+    if (high == true)
+        cout << " Priority |" << endl;
+    else
+        cout << endl;
+    cout << "+-------------+--------------";
+    cout << "+------------+-----------------";
+    cout << "+-----------------+--------------+---------------+";
+    if (high == true)
+        cout << "----------+" << endl;
+    else
+        cout << endl;
+    while (!main_queue.empty()) {
+        temp = to_string(main_queue.top().p_no).length();
+        cout << '|' << string(6 - temp / 2 - temp % 2, ' ')
+             << main_queue.top().p_no << string(7 - temp / 2, ' ');
+        temp = to_string(main_queue.top().start_AT).length();
+        cout << '|' << string(7 - temp / 2 - temp % 2, ' ')
+             << main_queue.top().start_AT << string(7 - temp / 2, ' ');
+        temp = to_string(main_queue.top().BT).length();
+        cout << '|' << string(6 - temp / 2 - temp % 2, ' ')
+             << main_queue.top().BT << string(6 - temp / 2, ' ');
+        temp = to_string(main_queue.top().CT).length();
+        cout << '|' << string(8 - temp / 2 - temp % 2, ' ')
+             << main_queue.top().CT << string(9 - temp / 2, ' ');
+        temp = to_string(main_queue.top().TAT).length();
+        cout << '|' << string(8 - temp / 2 - temp % 2, ' ')
+             << main_queue.top().TAT << string(9 - temp / 2, ' ');
+        temp = to_string(main_queue.top().WT).length();
+        cout << '|' << string(7 - temp / 2 - temp % 2, ' ')
+             << main_queue.top().WT << string(7 - temp / 2, ' ');
+        temp = to_string(main_queue.top().RT).length();
+        cout << '|' << string(7 - temp / 2 - temp % 2, ' ')
+             << main_queue.top().RT << string(8 - temp / 2, ' ');
+        if (high == true) {
+            temp = to_string(main_queue.top().priority).length();
+            cout << '|' << string(5 - temp / 2 - temp % 2, ' ')
+                 << main_queue.top().priority << string(5 - temp / 2, ' ');
+        }
+        cout << "|\n";
+        main_queue.pop();
+    }
+    cout << "+-------------+--------------";
+    cout << "+------------+-----------------";
+    cout << "+-----------------+--------------+---------------+";
+    if (high == true)
+        cout << "----------+";
+    cout << endl;
+    temp1 = get_total_CT(tempq);
+    cout << "\nTotal completion time :- " << temp1
+         << endl;
+    cout << "Average completion time :- " << temp1 / size
+         << endl;
+    temp1 = get_total_TAT(tempq);
+    cout << "\nTotal turnaround time :- " << temp1
+         << endl;
+    cout << "Average turnaround time :- " << temp1 / size
+         << endl;
+    temp1 = get_total_WT(tempq);
+    cout << "\nTotal waiting time :- " << temp1
+         << endl;
+    cout << "Average waiting time :- " << temp1 / size
+         << endl;
+    temp1 = get_total_RT(tempq);
+    cout << "\nTotal response time :- " << temp1
+         << endl;
+    cout << "Average response time :- " << temp1 / size
+         << endl;
 }
 
-void disp_gantt_chart(queue<process> gantt) {
+// Function to display Gantt Chart
+void disp_gantt_chart(queue<process> gantt)
+{
     int temp, prev = 0;
     queue<process> spaces = gantt;
-    cout << "\nGantt Chart (IS indicates idle state):\n\n+";
-    
+    cout << "\n\nGantt Chart (IS indicates ideal state) :- \n\n+";
+
+    // For 1st row of gantt chart
     while (!spaces.empty()) {
-        cout << string(2 * spaces.front().temp_BT, '-') << "+";
+        cout << string(to_string(spaces.front().p_no).length()
+                           + (spaces.front().p_no != -1)
+                           + 2 * spaces.front().temp_BT,
+                       '-')
+             << "+";
         spaces.pop();
     }
     cout << "\n|";
     spaces = gantt;
-    
+
+    // For process no. in 2nd row
     while (!spaces.empty()) {
         cout << string(spaces.front().temp_BT, ' ');
         if (spaces.front().p_no == -1)
             cout << "IS" << string(spaces.front().temp_BT, ' ') << '|';
         else
-            cout << "P" << spaces.front().p_no << string(spaces.front().temp_BT, ' ') << '|';
+            cout << "P" << spaces.front().p_no
+                 << string(spaces.front().temp_BT, ' ') << '|';
         spaces.pop();
     }
     spaces = gantt;
     cout << "\n+";
-    
+
     while (!spaces.empty()) {
-        cout << string(2 * spaces.front().temp_BT, '-') << "+";
+        cout << (string(to_string(spaces.front().p_no).length()
+                            + (spaces.front().p_no != -1)
+                            + 2 * spaces.front().temp_BT,
+                        '-'))
+             << "+";
         spaces.pop();
     }
     spaces = gantt;
     cout << "\n0";
-    
+//For 3rd row of gantt chart
     while (!spaces.empty()) {
         temp = to_string(spaces.front().CT).length();
-        cout << string(2 * spaces.front().temp_BT - temp / 2 - prev, ' ') << spaces.front().CT;
+        cout << (string(to_string(spaces.front().p_no).length()
+                            + (spaces.front().p_no != -1)
+                            + 2 * spaces.front().temp_BT - temp / 2 - prev,
+                        ' '))
+             << spaces.front().CT;
         prev = temp / 2 - temp % 2 == 0;
         spaces.pop();
     }
-    cout << "\n";
+    cout << "\n\n";
 }
 
-int main() {
-    priority_queue<process> ready_queue = set_sample_data();
+// Driver Code
+int main()
+{
+    // Initialise Ready and Completion Queue
+    priority_queue<process> ready_queue;
     priority_queue<process> completion_queue;
+
+    // queue for Gantt Chart
     queue<process> gantt;
+    ready_queue = set_sample_data();
+
+    // Function call for completion data
     completion_queue = FCFS_run(ready_queue, &gantt);
+
+    // Display Completion Queue
     disp(completion_queue, false);
+
+    // Display Gantt Chart
     disp_gantt_chart(gantt);
     return 0;
 }
